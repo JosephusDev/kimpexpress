f0bebfdcf53ddd08954fe53b7ece45e76c34773c
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-03-11 23:23:05 +0100

    Rota de SMS
    A rota que permite chamar a API de Vonage para enviar SMS.

--- index.js
+++ index.js
@@ -8,5 +8,28 @@ app.use(express.json())
 app.use(cors())
 app.use("/", veiculosRoutes)
 
+app.post('/sendSMS', async (req, res) => {
+    const { VONAGE_BRAND_NAME, TO_NUMBER, VONAGE_API_KEY, VONAGE_API_SECRET, DATA_AGENDADA, HORA_AGENDADA, UTENTE } = req.body;
+
+    const url = 'https://rest.nexmo.com/sms/json';
+    const params = `from=${VONAGE_BRAND_NAME}&text=Saudacoes Sr(a). ${UTENTE}, sua audiencia foi marcada para o dia ${DATA_AGENDADA} às ${HORA_AGENDADA}. Obrigado.\n&to=${TO_NUMBER}&api_key=${VONAGE_API_KEY}&api_secret=${VONAGE_API_SECRET}`;
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: params
+      });
+
+      const responseData = await response.json();
+      res.json(responseData);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
 app.listen(8800)
 console.log("Servidor rodando em http://localhost:8800/")
