f5f52dc1e665e68a4676abc59497abe4d71d3701
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-05-02 09:49:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,29 @@ app.post('/sendSMS', async (req, res) => {
       console.error(error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  });
+});
+
+app.post('/notificargpu', async (req, res) => {
+  const { VONAGE_BRAND_NAME, TO_NUMBER, VONAGE_API_KEY, VONAGE_API_SECRET, UTENTE, DATA, HORA, GABINETE } = req.body;
+
+  const url = 'https://rest.nexmo.com/sms/json';
+  const params = `from=${VONAGE_BRAND_NAME}&text=Saudacoes Sr(a). ${UTENTE}, sua audiencia foi marcada para o dia ${DATA} as ${HORA}, no gabinete: ${GABINETE}. Obrigado.\n&to=${TO_NUMBER}&api_key=${VONAGE_API_KEY}&api_secret=${VONAGE_API_SECRET}`;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
+    });
+
+    const responseData = await response.json();
+    res.json(responseData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
 app.listen(8800)
