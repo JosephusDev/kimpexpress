090688c9d7620ce04dc907e9384220ac9eab1ad1
José Conde Pinto <127633531+JosephusDev@users.noreply.github.com>
2024-06-19 09:08:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -31,29 +31,6 @@ app.post('/sendSMS', async (req, res) => {
     }
 });
 
-app.post('/smsENDE', async (req, res) => {
-    const { VONAGE_BRAND_NAME, TO_NUMBER, VONAGE_API_KEY, VONAGE_API_SECRET, UTENTE } = req.body;
-
-    const url = 'https://rest.nexmo.com/sms/json';
-    const params = `from=${VONAGE_BRAND_NAME}&text=Saudacoes Sr(a). ${UTENTE}, Tem uma conta de energia pendente, dirija-se a uma de nossas lojas para pagar. Obrigado.\n&to=${TO_NUMBER}&api_key=${VONAGE_API_KEY}&api_secret=${VONAGE_API_SECRET}`;
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: params
-      });
-
-      const responseData = await response.json();
-      res.json(responseData);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
 app.post('/notificargpu', async (req, res) => {
   const { VONAGE_BRAND_NAME, TO_NUMBER, VONAGE_API_KEY, VONAGE_API_SECRET, UTENTE, DATA, HORA, GABINETE } = req.body;
 
